c7306da482321d287bd4929e1728017235d5b080
Felipe Bruno <programming1129@gmail.com>
2020-01-09 07:36:13 -0300

    Add SellerService, FindAllAsync and Update View(Sellers)

---
+++ WebSales/Services/SellerService.cs
@@ -0,0 +1,25 @@
+﻿using WebSales.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebSales.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebSales.Services
+{
+    public class SellerService
+    {
+        private readonly WebSalesContext _Context;
+
+        public SellerService(WebSalesContext context)
+        {
+            _Context = context;
+        }
+
+        public async Task<List<Seller>> FindAllAsync()
+        {
+            return await _Context.Seller.ToListAsync();
+        }
+    }
+}
--- WebSales/Startup.cs
+++ WebSales/Startup.cs
@@ -10,6 +10,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.EntityFrameworkCore;
 using WebSales.Data;
+using WebSales.Services;
 
 namespace WebSales
 {
@@ -32,6 +33,7 @@ namespace WebSales
                 builder.MigrationsAssembly("WebSales")));
 
             services.AddScoped<SeedingService>();
+            services.AddScoped<SellerService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.

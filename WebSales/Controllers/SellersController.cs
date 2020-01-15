d6dc5a524cde11b60cf9e8a59d118f1a873a6f34
Felipe Bruno <programming1129@gmail.com>
2020-01-14 22:00:37 -0300

    Add View Details, Edit and Method to Update the Edit(Fix it)

--- WebSales/Controllers/SellersController.cs
+++ WebSales/Controllers/SellersController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using WebSales.Services;
+using WebSales.Services.Exceptions;
 using WebSales.Models;
 using WebSales.Data;
 using WebSales.Models.ViewModels;
@@ -14,11 +15,13 @@ namespace WebSales.Controllers
     {
         private readonly SellerService _sellerService;
         private readonly DepartmentService _departmentService;
+        private readonly WebSalesContext _webSalesContext;
 
-        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        public SellersController(SellerService sellerService, DepartmentService departmentService, WebSalesContext webSalesContext)
         {
             _sellerService = sellerService;
             _departmentService = departmentService;
+            _webSalesContext = webSalesContext;
         }
 
         public async Task<IActionResult> Index()
@@ -49,5 +52,91 @@ namespace WebSales.Controllers
             await _sellerService.InsertAsync(seller);
             return RedirectToAction(nameof(Index));
         }
+
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _sellerService.FinByIdAsync(Id.Value);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return View(result);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _sellerService.RemoveAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+
+        public async Task<IActionResult> Details(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _sellerService.FinByIdAsync(Id.Value);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            return View(result);
+        }
+
+        public async Task<IActionResult> Edit(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var result = await _sellerService.FinByIdAsync(Id.Value);
+
+            if (result == null)
+            {
+                return NotFound();
+            }
+
+            List<Department> departments = await _departmentService.FindAllAsync();
+            var viewModel = new SellerFormViewModel { Departments = departments, Seller = result };
+            return View(viewModel);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, Seller obj)
+        {
+            if (Id != obj.Id)
+            {
+                return BadRequest();
+            }
+
+            try
+            {
+               await _sellerService.UpdateAsync(obj);
+               return RedirectToAction(nameof(Index));
+            }
+            catch (NotFoundException)
+            {
+                return NotFound();
+            }
+            catch (DbConcurrencyException)
+            {
+                return BadRequest();
+            }
+        }
     }
+
 }
--- WebSales/Services/SellerService.cs
+++ WebSales/Services/SellerService.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using WebSales.Models;
+using WebSales.Services.Exceptions;
 using Microsoft.EntityFrameworkCore;
 
 namespace WebSales.Services
@@ -26,6 +27,36 @@ namespace WebSales.Services
         {
             _Context.Add(obj);
             await _Context.SaveChangesAsync();
+        }
+
+        public async Task<Seller> FinByIdAsync(int? id)
+        {
+            return await _Context.Seller.Include(obj => obj.Department).FirstOrDefaultAsync(obj => obj.Id == id);
+        }
+
+        public async Task RemoveAsync(int id)
+        {
+            var obj = _Context.Seller.Find(id);
+            _Context.Remove(obj);
+            await _Context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(Seller obj)
+        {
+            if (!_Context.Seller.Any(x => x.Id == obj.Id))
+            {
+                throw new NotFoundException("Id not Found");
+            }
+
+            try
+            {
+                _Context.Update(obj);
+                await _Context.SaveChangesAsync();
+            }
+            catch(DbConcurrencyException e)
+            {
+                throw new DbConcurrencyException(e.Message);
+            }
         }
     }
 }
